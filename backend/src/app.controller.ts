5ca915002d34365a587dc9ac17626bd856697bca
John Drake Brockman <john.drakebrockman@objego.de>
2023-05-03 11:34:28 +0200

    add code

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,7 +1,9 @@
 import {
   Controller,
   Get,
+  Param,
   Post,
+  Query,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
@@ -12,21 +14,51 @@ import * as tesseract from 'node-tesseract-ocr';
 
 @Controller()
 export class AppController {
+  links = {};
+
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  async getOcrViaUrl() {
-    const img = 'https://tesseract.projectnaptha.com/img/eng_bw.png';
-    const text = await tesseract.recognize(img);
-    return text;
-  }
-
-  @Post('ocr')
+  @Post('ocr/:id')
   @UseInterceptors(FileInterceptor('file'))
-  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+  async uploadFile(@UploadedFile() file: Express.Multer.File, @Param('id') id) {
     // const img = 'https://tesseract.projectnaptha.com/img/eng_bw.png';
     console.log(file);
     const text = await tesseract.recognize(file.buffer);
+
+    this.links[id] = text;
+
     return text;
+  }
+
+  @Get('link')
+  async getLink() {
+    let r = this.generateHash();
+    let safe = false;
+
+    while (!safe) {
+      if (this.links[r]) {
+        r = this.generateHash();
+      } else {
+        safe = true;
+      }
+    }
+
+    return r;
+  }
+
+  @Get('link/:id')
+  async getLinkById(@Param('id') id) {
+    if (this.links[id]) {
+      const value = this.links[id];
+      delete this.links[id];
+
+      return value;
+    }
+
+    return null;
+  }
+
+  private generateHash() {
+    return (Math.random() + 1).toString(36).substring(7);
   }
 }
